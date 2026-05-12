991af4a1c5178d5b8d8cd7dbc64322541bb75d99
Nicolas <nicolas.cataluna@gmail.com>
2026-05-12 15:16:24 +0200

    fix: redirection des requêtes /assets/ vers Symfony pour le bon fonctionnement d'AssetMapper
    Sans cette modification, Nginx cherchait les fichiers statiques directement sur le disque
    dans public/assets/ alors qu'AssetMapper les génère et les sert dynamiquement via Symfony.
    Cela provoquait des erreurs de type MIME incorrect (text/html au lieu de text/javascript)
    car Nginx retournait une page d'erreur 404 HTML à la place des fichiers JS/CSS attendus.

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\Chartjs\ChartjsBundle::class => ['all' => true],
+    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
