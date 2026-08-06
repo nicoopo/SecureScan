6d3f02836569a3f01ac34dccc44c52fa9416c339
nicolas <origicourt@proton.me>
2026-08-06 16:06:31 +0200

    feat: ajout de Bootstrap 5 via importmap
    Ajoute bootstrap, @popperjs/core et le CSS Bootstrap dans l'importmap.

--- importmap.php
+++ importmap.php
@@ -31,4 +31,14 @@ return [
     '@kurkle/color' => [
         'version' => '0.3.4',
     ],
+    'bootstrap' => [
+        'version' => '5.3.8',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
 ];
